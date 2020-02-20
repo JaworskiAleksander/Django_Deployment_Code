fcb3a110376f44d8a48a01dc9ba0f0e1feb79612
Aleksander Jaworski <aleck156@gmail.com>
2020-02-20 10:39:20 +0100

    created basic models

--- School_Management_System/CRUD_System/models.py
+++ School_Management_System/CRUD_System/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class School(models.Model):
+    name = models.CharField(max_length = 256)
+    location = models.CharField(max_length = 256)
+    
+class Principal(models.Model):
+    name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    # picture = models.ImageField()
+
+class Student(models.Model):
+    name = models.CharField(max_length = 256)
+    last_name = models.CharField(max_length = 256)
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='students')
