a4a35633f97eb18333095dffe1a3357e185d768f
Aleksander Jaworski <aleck156@gmail.com>
2020-02-20 10:43:09 +0100

    registered models in admin view

--- School_Management_System/CRUD_System/admin.py
+++ School_Management_System/CRUD_System/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from CRUD_System.models import School, Student, Principal
 
 # Register your models here.
+admin.site.register(School)
+admin.site.register(Principal)
+admin.site.register(Student)
