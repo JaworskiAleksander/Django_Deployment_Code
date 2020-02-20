28aaeb3e3685946b2319b8cc79bad0d7981f5848
Aleksander Jaworski <aleck156@gmail.com>
2020-02-20 10:09:04 +0100

    defined IndexView at views.py

--- School_Management_System/CRUD_System/views.py
+++ School_Management_System/CRUD_System/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views.generic import View, TemplateView
 
 # Create your views here.
+
+# this view is what users will see when loading main page
+class IndexView(TemplateView):
+    template_name = 'index.html'
