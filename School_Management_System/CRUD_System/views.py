ac937f17f8eaadfca6c3ea169c7c970fbb514d2b
Aleksander Jaworski <aleck156@gmail.com>
2020-02-21 09:55:32 +0100

    added *CreateView for students, schools and principals in views.py

--- School_Management_System/CRUD_System/views.py
+++ School_Management_System/CRUD_System/views.py
@@ -15,23 +15,39 @@ class CRUD_SystemIndex(TemplateView):
     template_name = 'CRUD_System/index.html'
 
 # this class is responsible for displaying list of all schools
+# School views
 class SchoolListView(ListView):
     context_object_name = 'schools'
     model = models.School
     template_name = 'CRUD_ListView.html'
 
+class SchoolCreateView(CreateView):
+    context_object_name = 'school_create'
+    model = models.School
+    fields = ('name', 'location')
 
 class SchoolDetailView(DetailView):
     context_object_name = 'school_details'
     model = models.School
 
+# Student views
 class StudentListView(ListView):
     context_object_name = 'students'
     model = models.Student
     template_name = 'CRUD_ListView.html'
 
+class StudentCreateView(CreateView):
+    context_object_name = 'student_create'
+    model = models.Student
+    fields = ('name', 'last_name', 'school')
 
+# Principal views
 class PrincipalListView(ListView):
     context_object_name = 'principals'
     model = models.Principal
     template_name = 'CRUD_ListView.html'
+
+class PrincipalCreateView(CreateView):
+    context_object_name = 'principal_create'
+    model = models.Principal
+    fields = ('name', 'last_name', 'school')
